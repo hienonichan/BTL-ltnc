c5fd5e4db21575cd301cc8728444629b2e313a55
hienonichan <hvu6582@gmail.com>
2024-03-09 23:04:46 +0700

    test library

--- BTL ltnc/main.cpp
+++ BTL ltnc/main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<SDL.h>
-
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIHGT = 640;
 int main(int argc, char* argv[]) {
-	std::cout << "hehe";
+	
+	SDL_Window* window = SDL_CreateWindow("hehe", 100, 100, WINDOW_WIDTH, WINDOW_HEIHGT, SDL_WINDOW_SHOWN);
+	SDL_Delay(2000);
+
 	return 0;
 }
